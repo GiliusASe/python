a69890387ae0c6a46d42bf99b1bb22e9b4f4b571
MarcCalvetCasas <98348722+MarcCalvetCasas@users.noreply.github.com>
2022-02-20 21:28:27 +0100

    Update Github.py

--- Github.py
+++ Github.py
@@ -1,6 +1,21 @@
-from tkinter import *
-def MenuOpcions():
-    print("MENÚ D'OPCIONS:")
-    print("1- VERIFICAR NOMBRES PRIMERS ")
-    print("2- DIBUIXAR UN TAULELL D'ESCACS")
-    print("3- CÀLCUL CONSUM")
+def DiesPassats():
+
+    import datetime
+    
+    diaN = int(input("Dia de Naixament:"))
+    mesN = int(input("Mes de Naixament:"))
+    anyN = int(input("Any de Naixament:"))
+
+    dActual = datetime.date.today()
+
+    print(
+    "dia" , dActual.day,
+    "mes" , dActual.month,
+    "any" , dActual.year)
+
+    diesPassats = dActual.day - diaN
+    mesosPassats = (dActual.month - mesN)*30
+    anysPassats = (dActual.year - anyN)*365
+
+    diesTotals = diesPassats + mesosPassats + anysPassats
+    print ("Han passat:", diesTotals, "dies")
