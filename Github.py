c9eb87ba7a3d23d12466274c90fa63e2fb31193c
DavidHinojosaMont <98348852+DavidHinojosaMont@users.noreply.github.com>
2022-02-20 22:48:43 +0100

    Update Github.py

--- Github.py
+++ Github.py
@@ -1,38 +1,55 @@
-#DECLARACIÓ DE FUNCIONS
-from os import *
-from time import sleep
+def AreaTriangle():
 
-def MenuOpcions():
-    print("MENÚ D'OPCIONS:")
-    print("0- SORTIR DEL PROGRAMA")
-    print("1- VERIFICAR NOMBRES PRIMERS ")
-    print("2- DIBUIXAR UN TAULELL D'ESCACS")
-    print("3- CÀLCUL CONSUM")
+    base= float(input("Introdueix la base del triangle: "))
+    altura= float(input("Introdueix l'alçada del triangle: "))
 
-#MAIN
-MenuOpcions()
-tria = input("ESCULL UNA OPCIÓ: ")
-while (tria != '0'):
-    if (tria == '1'):
-        NombresPrimers()
-    elif (tria == '2'):
-        TaulellEscacs()
-    elif (tria == '3'):
-        CalculConsum()
-    elif (tria == '4'):
-        DiesPassats()
-    elif (tria == '5'):
-        OrdenarNumeros()
-    elif (tria == '6'):
-        CalculadorsBasica()
-    else: 
-        print("ERROR")
-        sleep(1)
-        
-    #RETORN AL MENÚ
-    print("TORNANT AL MENÚ PRINCIPAL")
-    #NETEJA DE PANTALLA
-    sleep(0.7)
-    system('cls')
-    #TRIA D'OPCIÓ
-    tria = input("ESCULL UNA OPCIÓ: ")
+    area= base*altura
+    area2= area/2
+
+    perimetre= ((base+altura)*2)/2
+
+    print("L'àrea del triangle es ", area2, "i el seu perimetre es ", perimetre)
+
+def DiesPassats():
+    
+    import datetime
+    datanaixementdia = int(input("Dia en el que vas nèixer:"))
+    datanaixementmes = int(input("Mes de naixement:"))
+    datanaixementany = int(input("Any de naixement:"))
+
+    dataActual = datetime.date.today()
+    #print(dataActual)
+    #print(type.dataActual)
+
+    print("dia", dataActual.day, "mes", dataActual.month, "any", dataActual.year)
+
+    diespassats = dataActual.day - datanaixementdia
+    mesospassats = dataActual.month - datanaixementmes
+    anyspassats = dataActual.year - datanaixementany
+
+    resultatfinal = diespassats + mesospassats*30 + anyspassats*360
+
+    print("des de que vas nèixer, han passat", resultatfinal, "dies.")
+    
+def CalculOperacio():
+    
+    operand1 = float(input("Introdueix el primer número: "))
+    operand2 = float(input("Introdueix el segon número: "))
+    operador = input("Introdueix un operador: ")
+
+    if operador == "+" or operador == "-" or operador == "" or operador == "/" :
+
+        if operador == "+":
+            print(operand1 + operand2)
+
+        if operador == "-":
+            print(operand1 - operand2)
+
+        if operador == "":
+            print(operand1 * operand2)
+
+        if operador == "/":
+            print(operand1 / operand2)
+
+    else :
+        print("El caràcter introduït no és un operador")
