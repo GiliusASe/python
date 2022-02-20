210a825541a0c5536b55cc18a7825eb8ffd27075
Gil <a2106874@institutmontilivi.cat>
2022-02-20 20:32:25 +0100

    Programa nombresPrimers

--- Github.py
+++ Github.py
@@ -5,3 +5,17 @@ def MenuOpcions():
     print("2- DIBUIXAR UN TAULELL D'ESCACS")
     print("3- CÀLCUL CONSUM")
 
+def NombresPrimers():
+    print("EXECUTANT VERIFICADOR DE NOBRES PRIMERS:")
+    primer = True
+    divisor = 2
+
+            
+    numero = int(input("ENTRA UN NOMBRE"))
+    numero = int(numero)
+    while divisor < numero:
+        if ((numero % divisor) == 0):primer = False
+        divisor = divisor + 1
+
+    if primer == True:print("EL NOMBRE ",numero," ÉS PRIMER")
+    else:print("EL NOMBRE ",numero," NO ÉS PRIMER")
