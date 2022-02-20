bdd0f204fb285f3d2816a03ff9122b5ab65858cd
Gil <a2106874@institutmontilivi.cat>
2022-02-20 21:17:37 +0100

    Menú Funcional

--- Github.py
+++ Github.py
@@ -1,6 +1,25 @@
-from tkinter import *
+#DECLARACIÓ DE FUNCIONS
+from os import *
+from time import sleep
+
 def MenuOpcions():
     print("MENÚ D'OPCIONS:")
     print("1- VERIFICAR NOMBRES PRIMERS ")
     print("2- DIBUIXAR UN TAULELL D'ESCACS")
     print("3- CÀLCUL CONSUM")
+
+#MAIN
+MenuOpcions()
+tria = input("ESCULL UNA OPCIÓ: ")
+while (tria != '0'):
+    if (tria == '1'):
+        NombresPrimers()
+    elif (tria == '2'):
+        TaulellEscacs()
+    elif (tria == '3'):
+        CalculConsum()
+    else: 
+        print("ERROR, torna a intentar;")
+        sleep(1)
+    system('cls')
+    tria = input("ESCULL UNA OPCIÓ: ")
