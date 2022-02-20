7b51354efdeb24941a3408d8dd686ff78d63614d
Gil <a2106874@institutmontilivi.cat>
2022-02-20 20:28:36 +0100

    Model menú d'opcions

---
+++ Github.py
@@ -0,0 +1,7 @@
+from tkinter import *
+def MenuOpcions():
+    print("MENÚ D'OPCIONS:")
+    print("1- VERIFICAR NOMBRES PRIMERS ")
+    print("2- DIBUIXAR UN TAULELL D'ESCACS")
+    print("3- CÀLCUL CONSUM")
+
